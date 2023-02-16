208b93e2c5f6aa1697f47246021477da098eedfa
Vherniel Lebis <hello@vherniellebis.tech>
2023-02-17 05:24:45 +0800

    test: test deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,16 @@
 
 <section>
     <h1>Home Page</h1>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolorum voluptatibus eveniet aliquam, officia nulla?
+        Deserunt, sequi fugit! Soluta asperiores similique cum ab eum ex maiores hic dolor nihil ea?
+    </p>
 </section>
 
 <style lang="scss">
+    p {
+        font-size: 4rem;
+        font-weight: bold;
+        color: red;
+    }
 </style>
