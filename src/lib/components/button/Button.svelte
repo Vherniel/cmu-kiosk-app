15827865b9b8d71613ba384bd7b15c957210c296
Vherniel Lebis <hello@vherniellebis.tech>
2023-02-17 04:33:36 +0800

    Initial commit

---
+++ src/lib/components/button/Button.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import type { SvelteComponent } from "svelte";
+    // export let prefix: any;
+    // export let suffix: any;
+    export let icon: any;
+</script>
+
+<!-- TODO: Tooltip -->
+<button class="button">
+    <div class="button-layout">
+        {#if typeof icon == "string"}
+            icon
+        {:else}
+            <svelte:component this={icon} />
+        {/if}
+
+        <span class="label"><slot /></span>
+    </div>
+</button>
+
+<style lang="scss">
+    .button-layout {
+        display: flex;
+        align-items: center;
+    }
+    .button {
+        border: 0;
+        border-radius: 0.25em;
+        padding-top: 1em;
+        padding-bottom: 1em;
+        padding-left: 1.5em;
+        padding-right: 1.5em;
+        font-weight: bold;
+        font-size: 0.875rem;
+        cursor: pointer;
+    }
+    .button[disabled] {
+        cursor: not-allowed;
+    }
+</style>
---
+++ src/routes/components/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Button from "$lib/components/button/Button.svelte";
+    import DiJsBadge from "svelte-icons/di/DiJsBadge.svelte";
+</script>
+
+<div>
+    <!-- iconSize={1} -->
+    <Button icon={DiJsBadge}>Button text</Button>
+</div>
+
+<style lang="scss">
+</style>
