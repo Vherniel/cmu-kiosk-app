ae5d093bddd05293db2f3eb87601feab7a9be319
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-03 07:48:22 +0800

    feat: add new styles and features with additional comments

--- src/lib/components/card-button/CardButton.svelte
+++ src/lib/components/card-button/CardButton.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
     import type { ComponentType } from "svelte";
-    import type { Icon } from "lucide-svelte";
-    import { BoxSelect } from "lucide-svelte";
+    import { type Icon, BoxSelect, Check } from "lucide-svelte";
 
     export let href: null | string = null;
     export let icon: ComponentType<Icon> = BoxSelect;
     export let disabled: boolean = false;
-    export let label: string = "Lorem ipsum dolor sit amet";
+    export let label: string = "Lorem ipsum dolor sit amet consectetur";
 </script>
 
 <!-- default -->
@@ -21,26 +20,70 @@
 <svelte:element></svelte:element> -->
 
 <svelte:element this={href ? "a" : "button"} class={"card"} {href} {disabled}>
-    <div>
+    <div class="header">
         <div>
-            <svelte:component this={icon} />
+            <svelte:component this={icon} size="32" />
         </div>
-        <div>{!disabled ? "Available" : "Unavailable"}</div>
+        <div class="status-text">{!disabled ? "Available" : "Unavailable"}</div>
     </div>
 
-    <div>
-        <div>{label}</div>
-        <!-- <div>Indicator</div> -->
+    <div class="footer">
+        <div class="label">{label}</div>
+        <!-- 
+            Statuses:
+                Acquired             -- Check
+                Available            -- Triangle
+                Unavailable          -- Cross and greyed
+                Unavailable:Acquired -- Check and greyed
+        -->
+        <div class="status-icon"><Check color="#fff" size="18" /></div>
     </div>
 </svelte:element>
 
 <style lang="scss">
     .card {
-        display: inline-block;
+        display: flex;
+        flex-direction: column;
+        background-color: #ffffff;
         border: solid 1px #d9d9d9;
         border-radius: 0.5rem;
         padding: 1.25rem;
-        background-color: #ffffff;
+        color: #000;
+        text-decoration: none;
         cursor: pointer;
+        height: 12rem;
+        width: 100%;
+    }
+    .header {
+        display: flex;
+        align-items: center;
+        .status-text {
+            margin-left: auto;
+        }
+    }
+    .footer {
+        padding-top: 4rem;
+        display: flex;
+        align-items: center;
+        margin-top: auto;
+        .label {
+            margin-right: 2rem;
+            font-size: 1.25rem;
+            font-weight: var(--font-bold);
+        }
+        .status-icon {
+            margin-left: auto;
+            border-radius: 999px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #000;
+            min-width: 1.5rem;
+            min-height: 1.5rem;
+            max-width: 1.5rem;
+            max-height: 1.5rem;
+            width: 1.5rem;
+            height: 1.5rem;
+        }
     }
 </style>
