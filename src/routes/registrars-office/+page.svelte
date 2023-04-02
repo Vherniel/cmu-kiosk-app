61a2d11bd99f1f73d759a154ab0b1ef9c2b8d7c0
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-03 07:42:45 +0800

    feat: add CardButton components

--- src/routes/registrars-office/+page.svelte
+++ src/routes/registrars-office/+page.svelte
@@ -1,12 +1,54 @@
 <script lang="ts">
     import CardButton from "$lib/components/card-button/CardButton.svelte";
+    import { Building2, RefreshCw } from "lucide-svelte";
 </script>
 
 <section>
     <h1>Registrar’s Office</h1>
-    <div>
-        <CardButton href="/" />
+    <p>Select a form to continue</p>
+    <div class="contents">
+        <div class="flex">
+            <div class="flex-item">
+                <CardButton
+                    href="/registrars-office/admission"
+                    label="CMU Admission"
+                    icon={Building2} />
+            </div>
+            <div class="flex-item">
+                <CardButton
+                    href="/registrars-office/application-to-shift-course"
+                    label="Application to Shift Course"
+                    icon={RefreshCw} />
+            </div>
+            <div class="flex-item">
+                <CardButton href="/" />
+            </div>
+            <div class="flex-item">
+                <CardButton href="/" />
+            </div>
+            <div class="flex-item">
+                <CardButton href="/" />
+            </div>
+            <div class="flex-item">
+                <CardButton href="/" />
+            </div>
+            <div class="flex-item">
+                <CardButton href="/" />
+            </div>
+        </div>
     </div>
 </section>
 
-<style lang="scss"></style>
+<style lang="scss">
+    .contents {
+        .flex {
+            display: flex;
+            flex-flow: row wrap;
+            margin: -0.5rem;
+            .flex-item {
+                flex-basis: calc(100% / 4);
+                padding: 0.5rem;
+            }
+        }
+    }
+</style>
