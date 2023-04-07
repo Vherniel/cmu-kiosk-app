a7d28f7e9722d8064869aa14bf71ff2729148f73
Vherniel <vherniel11@gmail.com>
2023-04-07 23:26:29 +0800

    feat: move component declarations to array and rendered in a loop

--- src/routes/registrars-office/+page.svelte
+++ src/routes/registrars-office/+page.svelte
@@ -1,6 +1,57 @@
 <script lang="ts">
     import CardButton from "$lib/components/card-button/CardButton.svelte";
     import { Building2, RefreshCw } from "lucide-svelte";
+    import { prevent_default } from "svelte/internal";
+
+    // TODO: icon props
+
+    const forms = [
+        {
+            href: "/registrars-office/admission",
+            label: "CMU Admission",
+            icon: Building2,
+        },
+        {
+            href: "/registrars-office/application-to-shift-course",
+            label: "Application to Shift Course",
+            icon: RefreshCw,
+        },
+        {
+            href: "/registrars-office/leave-of-absence",
+            label: "Leave of Absence",
+            icon: RefreshCw,
+        },
+        {
+            href: "/registrars-office/request-form",
+            label: "Request Form",
+            icon: RefreshCw,
+        },
+        {
+            href: "/registrars-office/application-form-for-graduation",
+            label: "Application Form for Graduation",
+            icon: RefreshCw,
+        },
+        {
+            href: "/registrars-office/request-for-transcript-of-records-or-f137-from-former-school",
+            label: "Request for Transcript of Records or F137 from Former School",
+            icon: Building2,
+        },
+        {
+            href: "/registrars-office/entrance-examination-permit",
+            label: "Entrance Examination Permit",
+            icon: Building2,
+        },
+        {
+            href: "/registrars-office/requirement-to-enrolled",
+            label: "Requirement to Enrolled",
+            icon: Building2,
+        },
+        {
+            href: "/registrars-office/acknowledgement-receipt",
+            label: "Acknowledgement Receipt",
+            icon: Building2,
+        },
+    ];
 </script>
 
 <section>
@@ -8,33 +59,11 @@
     <p>Select a form to continue</p>
     <div class="contents">
         <div class="flex">
-            <div class="flex-item">
-                <CardButton
-                    href="/registrars-office/admission"
-                    label="CMU Admission"
-                    icon={Building2} />
-            </div>
-            <div class="flex-item">
-                <CardButton
-                    href="/registrars-office/application-to-shift-course"
-                    label="Application to Shift Course"
-                    icon={RefreshCw} />
-            </div>
-            <div class="flex-item">
-                <CardButton href="/" />
-            </div>
-            <div class="flex-item">
-                <CardButton href="/" />
-            </div>
-            <div class="flex-item">
-                <CardButton href="/" />
-            </div>
-            <div class="flex-item">
-                <CardButton href="/" />
-            </div>
-            <div class="flex-item">
-                <CardButton href="/" />
-            </div>
+            {#each forms as form}
+                <div class="flex-item">
+                    <CardButton {...form} on:click={() => {}} />
+                </div>
+            {/each}
         </div>
     </div>
 </section>
