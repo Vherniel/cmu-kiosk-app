1490b6bbdbf0b0077b7d428be19df6acb95b418a
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-13 14:43:51 +0800

    feat: change component composition style to slots

--- src/lib/layouts/sidebar/Sidebar.svelte
+++ src/lib/layouts/sidebar/Sidebar.svelte
@@ -1,11 +1,29 @@
-<script lang="ts"></script>
+<script lang="ts">
+    export let slotTop: string = "";
+    export let slotMiddle: string = "";
+    export let slotBottom: string = "";
+</script>
 
-<aside>
-    <slot />
-</aside>
+<div class={`flex flex-col h-full ${$$props.class}`.trim()}>
+    {#if $$slots.top}
+        <div class={`${slotTop}`.trim()}>
+            <slot name="top" />
+        </div>
+    {/if}
+    {#if $$slots.middle}
+        <div class={`${slotMiddle}`.trim()}>
+            <slot name="middle" />
+        </div>
+    {/if}
+    {#if $$slots.bottom}
+        <div class={`${slotBottom}`.trim()}>
+            <slot name="bottom" />
+        </div>
+    {/if}
+</div>
 
 <style lang="scss">
-    aside {
+    /* aside {
         background-color: #fafafa;
         flex-shrink: 0;
         flex-basis: 20rem;
@@ -16,5 +34,5 @@
         height: 100%;
         overflow-x: hidden;
         overflow-y: scroll;
-    }
+    } */
 </style>
