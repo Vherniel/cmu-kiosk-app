295d7e2e5533cd7e091b02329518e949e9862280
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-13 14:47:16 +0800

    feat: change component composition style to slots

--- src/lib/layouts/header/Header.svelte
+++ src/lib/layouts/header/Header.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
 </script>
 
-<header on:wheel|preventDefault|stopPropagation|nonpassive>
-    <slot />
+<header class={`flex items-center ${$$props.class}`.trim()}>
+    <div class="mr-auto">
+        <slot name="left" />
+    </div>
+    <div>
+        <slot name="right" />
+    </div>
 </header>
 
 <style lang="scss">
-    header {
-        position: sticky;
-        top: 0;
-        left: 0;
-        z-index: 10;
-        display: flex;
-        align-items: center;
-        background-color: #fff;
-        padding-top: 2rem;
-        padding-bottom: 2rem;
-        padding-left: 5.125rem;
-        padding-right: 5.125rem;
-    }
 </style>
