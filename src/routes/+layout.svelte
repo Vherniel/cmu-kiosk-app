e2affa8cddb1ba391393dea8bfe8ba15ec9c147a
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-13 15:04:10 +0800

    feat: implement popups

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+    import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
+    import { storePopup } from "@skeletonlabs/skeleton";
+    storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
 <slot />
