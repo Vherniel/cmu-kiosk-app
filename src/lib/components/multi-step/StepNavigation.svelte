0f4a3f45c0b646ce0671fd4c837f201d84570b38
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-15 05:13:04 +0800

    feat: update visual design and disabled functionality

--- src/lib/components/multi-step/StepNavigation.svelte
+++ src/lib/components/multi-step/StepNavigation.svelte
@@ -6,27 +6,37 @@
 
     import { getContext } from "svelte";
     import { key } from "./MultiStep.svelte";
+    import { ArrowLeft, ArrowRight } from "lucide-svelte";
 
     // @ts-ignore
     const { steps, selectedStep, nextStep, previousStep } = getContext(key);
 
-    export let previous: boolean = true;
-    export let next: boolean = true;
+    const previousDisabled = !$selectedStep.previous || $selectedStep == steps[0];
+    const nextDisabled = !$selectedStep.next || $selectedStep == steps[steps.length - 1];
 </script>
 
-<div class={"step-navigation"}>
-    {#if previous}
+<div class="mt-12 border-t-4 border-t-surface-50/30">
+    <div class="-mx-4 flex h-32">
         <button
-            class="previous"
-            disabled={$selectedStep == steps[0]}
-            on:click={() => previousStep()}>Previous</button>
-    {/if}
-    {#if next}
+            class="previous flex-1 m-4 text-2xl{previousDisabled ? ' text-surface-300' : ''}"
+            disabled={previousDisabled}
+            on:click={() => previousStep()}>
+            <div class="flex justify-center items-center">
+                <span class="mx-1"><ArrowLeft /></span>
+                <span class="mx-1">Previous</span>
+            </div>
+        </button>
+
         <button
-            class="next"
-            disabled={$selectedStep == steps[steps.length - 1]}
-            on:click={() => nextStep()}>Next</button>
-    {/if}
+            class="next flex-1 m-4 text-2xl{nextDisabled ? ' text-surface-300' : ''}"
+            disabled={nextDisabled}
+            on:click={() => nextStep()}>
+            <div class="flex justify-center items-center">
+                <span class="mx-1">Next</span>
+                <span class="mx-1"><ArrowRight /></span>
+            </div>
+        </button>
+    </div>
 </div>
 
 <style lang="scss">
