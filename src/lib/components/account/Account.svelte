aaf3cabb6c294420eaf0966b91d841ea581af49b
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-20 07:13:26 +0800

    feat: use utility css

--- src/lib/components/account/Account.svelte
+++ src/lib/components/account/Account.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     import { PUBLIC_KIOSK_GUEST_EMAIL } from "$env/static/public";
 
-    import { User } from "lucide-svelte";
+    import { UserIcon } from "lucide-svelte";
 
-    import type { LayoutData } from "../../../routes/$types";
+    import type { LayoutData } from "../../../routes/(main)/$types";
 
     export let img: string | null = null;
     export let data: LayoutData;
+
     $: ({ supabase, session } = data);
 
     $: user = data?.session?.user;
@@ -14,63 +15,77 @@
     let isSignedOut: boolean | null = null;
 
     $: isSignedOut = !session || user?.email == PUBLIC_KIOSK_GUEST_EMAIL;
+
+    let signedIn = true;
 </script>
 
-<a
-    href={isSignedOut ? "/signin" : "javascript:void(0)"}
-    class="account"
-    on:click={(e) => {
-        if (!isSignedOut) {
-            // if signed in
-            e.preventDefault();
-        }
-    }}>
-    <div class="img">
-        <div>
-            {#if img}
-                <img src="" alt="Guest" />
+<a href="/signin" class="btn w-full h-52 ring-1 flex flex-col p-4">
+    <div>
+        <div class="rounded-full overflow-hidden w-20 h-20 bg-secondary-400-500-token">
+            {#if !signedIn}
+                <UserIcon size="36" />
             {:else}
-                <User />
+                <img src="/images/cmu.png" class="block w-full h-full" alt="CMU" />
             {/if}
         </div>
     </div>
-    <!-- TODO: not yet dynamic. data is static. not yet from supabase table -->
-    <p class="account__name">{isSignedOut ? "Guest" : "Admin"}</p>
-    <p class="account__id">
-        {isSignedOut ? "ID: kiosk.guest" : "ID: kiosk.admin"}
-    </p>
+    <div class="mt-4">
+        <div class="font-extrabold">Guest</div>
+        <div>Login to save progress</div>
+    </div>
 </a>
 
-<style>
-    .account {
-        display: block;
-        color: unset;
-        text-decoration: none;
-        text-align: center;
-        border-radius: 0.75rem;
-        border: solid 1px #d9d9d9;
-        padding-top: 2rem;
-        padding-bottom: 2rem;
-    }
-    .img {
-        margin-left: auto;
-        margin-right: auto;
-        margin-bottom: 1rem;
-        width: 6rem;
-        height: 6rem;
-        background-color: #cccccc;
-        overflow: hidden;
-        border-radius: 999px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    .account__name,
-    .account__id {
-        margin-top: 0;
-        margin-bottom: 0;
-    }
-    .account__name {
-        font-weight: var(--font-bold);
-    }
+<div>
+    <!-- form should be <slot /> -->
+    <!-- <div class="rounded-lg border-2 border-secondary-500" data-popup="examplePopup">
+        <div class="relative bg-secondary-700 p-4 z-10">
+            <form action="" method="POST"></form>
+        </div>
+        <div class="arrow p-5 border-2 border-secondary-500 bg-secondary-700 z-0" />
+    </div> -->
+    <!-- make this a popup by default -->
+    <!-- when signed out -->
+    <!-- signin button, -->
+
+    <!-- <svelte:element
+        this={isSignedOut ? "a" : "button"}
+        href={isSignedOut ? "/signin" : null}
+        class="account"> -->
+    <!-- 
+            <Account
+                {session}
+                
+            >
+
+        -->
+
+    <!-- TODO: data flow? how? -->
+    <!--
+            if signed in,
+                should have NO borders and background
+                should be a button
+
+            if signed out,
+                should have borders and background
+                should be a a[href]
+        -->
+
+    <!-- <div class="img">
+            <div>
+                {#if img}
+                    <img src="" alt="Guest" />
+                {:else}
+                    <User />
+                {/if}
+            </div>
+        </div> -->
+    <!-- TODO: not yet dynamic. data is static. not yet from supabase table -->
+    <!-- <p class="account__name">{isSignedOut ? "Guest" : "Admin"}</p>
+        <p class="account__id">
+            {isSignedOut ? "ID: kiosk guest" : "ID: kiosk admin"}
+        </p>
+    </svelte:element> -->
+</div>
+
+<style lang="scss">
 </style>
