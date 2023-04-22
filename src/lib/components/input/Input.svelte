12aea2b70ad55874a314eaa3ca8faaab262e3624
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-22 11:22:08 +0800

    fix: add on:focus event and fix keyboardStore value

--- src/lib/components/input/Input.svelte
+++ src/lib/components/input/Input.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-    import { keyboard } from "$lib/components/keyboard-component";
-    import { afterUpdate, onMount } from "svelte";
+    import { keyboard, keyboardStore } from "$lib/components/keyboard-component";
+    import { afterUpdate } from "svelte";
     // interface $$Props extends HTMLInputElement {}
 
     // Superforms
     export let superform: any;
     export let name: string;
+    let element: any;
     $: value = "";
 
     $: ({ form, errors, constraints } = superform);
 
     afterUpdate(() => {
-        // console.log("form", $form[name]);
-        // console.log("value", value);
-        $form[name] = value;
+        if ($keyboardStore.input == element) {
+            $form[name] = $keyboardStore.value;
+        }
     });
 
     function typeProp(node: HTMLInputElement) {
@@ -22,12 +23,14 @@
 </script>
 
 <input
+    on:focus
     use:keyboard={value}
     {...$$restProps}
     {...$constraints[name]}
     class="bg-transparent w-full border-0 px-1 py-3 focus:ring-0 focus-visible:outline-none {$$props.class}"
     data-invalid={$errors[name]}
     bind:value
+    bind:this={element}
     use:typeProp />
 
 <style lang="scss">
