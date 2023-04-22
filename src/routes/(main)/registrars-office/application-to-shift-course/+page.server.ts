331eeeb7ae6c22a889a862c5c6c8ac2abdb43ae0
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-22 11:18:51 +0800

    feat: print data when in summary page

--- src/routes/(main)/registrars-office/application-to-shift-course/+page.server.ts
+++ src/routes/(main)/registrars-office/application-to-shift-course/+page.server.ts
@@ -6,39 +6,13 @@ import type { Actions, PageServerLoad } from "./$types";
 import { Printer } from "@node-escpos/core";
 import USB from "@node-escpos/usb-adapter";
 
-const device = new USB();
-
-device.open(async function (err) {
-    if (err) throw new Error("Error", { cause: err });
-
-    let printer = new Printer(device, { encoding: "GB18030" });
-
-    printer
-        .font("a")
-        .align("lt") // align to left
-        .style("b") // text bold
-        .size(1, 1)
-        .text("City of Malabon University");
-    // .table(["One", "Two", "Three"])
-    // .tableCustom(
-    //     [
-    //         { text: "Left", align: "LEFT", width: 0.33, style: "B" },
-    //         { text: "Center", align: "CENTER", width: 0.33 },
-    //         { text: "Right", align: "RIGHT", width: 0.33 },
-    //     ],
-    //     { encoding: "cp857", size: [1, 1] } // Optional
-    // );
-
-    printer.cut().close();
-});
-
 // TODO: Validation
 const formSchema = z.object({
     "First name": z.string(),
     "Middle name": z.string(),
     "Last name": z.string(),
-    "Student ID": z.number(),
-    Year: z.number(),
+    "Student ID": z.string(),
+    Year: z.string(),
     Section: z.string(),
     "School year": z.string(),
     "Former course": z.string(),
@@ -69,6 +43,98 @@ export const load = (async (event) => {
                 .select("*")
                 .eq("id", formRecordsId);
 
+            const { Signature, ...formData } = summary[0].form_values.superform.data;
+            const { paid, draft, released, paidBy } = summary[0].form_values.metadata;
+
+            function wrap(s: any, w: any) {
+                return s.replace(
+                    new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, "g"),
+                    "$1\n"
+                );
+            }
+
+            const orderedBy = [
+                "First name",
+                "Middle name",
+                "Last name",
+                "Student ID",
+                "Year",
+                "Section",
+                "School year",
+                "Former course",
+                "Former course major",
+                "New course",
+                "New course major",
+                "Reason for shifting",
+            ];
+
+            const parsedText = Object.entries(formData)
+                .sort(([a]: any, [b]: any) => orderedBy.indexOf(a) - orderedBy.indexOf(b))
+                .map(([label, value]) => {
+                    return `${wrap(label.toString(), 32)}: ${wrap(value.toString(), 32)}\n\n`;
+                })
+                .join("");
+
+            const device = new USB();
+
+            setTimeout(() => {
+                device.open(async function (err) {
+                    if (err) throw new Error("Error", { cause: err });
+
+                    let printer = new Printer(device, { encoding: "GB18030", width: 32 });
+
+                    printer
+                        .font("a")
+                        .align("LT") // align to left
+                        .style("b") // text bold
+                        .size(2, 3)
+                        .text("CMU Kiosk")
+                        .size(1, 1)
+                        .text("")
+                        .text("City of Malabon University")
+                        .text("College of Computer Studies")
+                        .text("")
+                        .text("ID: " + formRecordsId)
+                        .text("")
+                        .drawLine()
+                        .text("")
+                        .text(parsedText)
+                        .text("")
+                        .tableCustom([
+                            {
+                                text: paid ? "Paid by" : "Unpaid",
+                                width: 0.5,
+                            },
+                            {
+                                text: `${paidBy}`,
+                                width: 0.5,
+                            },
+                        ])
+                        .tableCustom([
+                            {
+                                text: "Price",
+                                width: 0.5,
+                            },
+                            {
+                                text: "P100.00",
+                                width: 0.5,
+                            },
+                        ])
+                        .text("")
+                        .drawLine()
+                        .text("")
+                        .text(
+                            wrap(
+                                "Please wait to call your ID or name and show this receipt to the registrar staff for further instructions.",
+                                32
+                            )
+                        )
+                        .text("");
+
+                    printer.cut().close();
+                });
+            }, 3000);
+
             return {
                 submitForm,
                 payment,
@@ -98,8 +164,9 @@ export const actions = {
                 {
                     form_values: {
                         metadata: {
-                            done: false,
+                            paid: false,
                             draft: true,
+                            released: false,
                         },
                         superform: form,
                     },
@@ -139,40 +206,43 @@ export const actions = {
                 returnUrl: event.url.href,
             });
 
-            const { data: formValues } = await event.locals.supabase
+            const { data: formValues } = await supabase
                 .from("registrar_form_records")
                 .select("form_values")
                 .eq("id", payment.data.formRecordsId);
 
-            formValues[0].form_values.metadata.done = true;
-            formValues[0].form_values.metadata.draft = false;
+            const metadata = formValues[0].form_values.metadata;
 
-            await event.locals.supabase
+            metadata.paidBy = "GCash";
+            metadata.paid = true;
+            metadata.draft = false;
+            metadata.released = false;
+
+            await supabase
                 .from("registrar_form_records")
                 .update({ form_values: formValues[0].form_values })
                 .eq("id", payment.data.formRecordsId);
 
-            const { data, error } = await event.locals.supabase.from("transactions").insert([
+            await supabase.from("transactions").insert([
                 {
                     registrar_form_records_id: payment.data.formRecordsId,
-                    is_paid: true,
-                    price: 10000,
+                    paid: true,
+                    price_by: "GCash",
+                    released: false,
+                    price: 100,
                 },
             ]);
 
-            // create transaction data to the database and set it to paid
-            // update form_values in registrar_form_records object metadata of done -> true, draft -> false
-
             if (action?.url) throw redirect(303, action.url);
-            // what to do after redirecting?
         }
 
         if (payment.data.Method == "cash") {
             const { data, error } = await supabase.from("transactions").insert([
                 {
                     registrar_form_records_id: payment.data.formRecordsId,
-                    is_paid: false,
-                    price: 10000,
+                    paid: false,
+                    paid_by: false,
+                    price: 100,
                 },
             ]);
             // create transaction data to the database and set it to unpaid
