7d0ed1ab23157ad96a76d51deddfd6cf89815682
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-22 11:19:15 +0800

    feat: add signout function

--- src/routes/(main)/profile/+page.svelte
+++ src/routes/(main)/profile/+page.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
+    import { enhance } from "$app/forms";
+    import { goto } from "$app/navigation";
+    import type { PageData } from "./$types";
+
+    export let data: PageData;
+
+    $: ({ supabase } = data);
+
+    async function handleLogout() {
+        const { error } = await supabase.auth.signOut();
+        throw new Error("Signout error: ", { cause: error });
+    }
 </script>
 
 <section>
     <div class="container">
-        <h1>Profile page</h1>
+        <div class="my-8"><h1>Profile page</h1></div>
+
+        <button
+            type="button"
+            class="btn variant-filled-error font-extrabold"
+            on:click={async () => {
+                await handleLogout();
+                goto("/");
+            }}
+            on:keyup={async () => {
+                await handleLogout();
+                goto("/");
+            }}>Logout</button>
     </div>
 </section>
 
