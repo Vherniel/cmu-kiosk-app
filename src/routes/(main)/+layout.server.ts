e13720aaacfc845d3c54523106cf04f637a44a73
Vherniel Lebis <hello@vherniellebis.tech>
2023-04-27 18:57:39 +0800

    feat: add new data on LayoutServerLoad

--- src/routes/(main)/+layout.server.ts
+++ src/routes/(main)/+layout.server.ts
@@ -1,13 +1,27 @@
-import { PUBLIC_CMU_WP_REST_ENDPOINT } from "$env/static/public";
+import { PRIVATE_WEATHERAPI_KEY } from "$env/static/private";
 import type { LayoutServerLoad } from "./$types";
 
-export const load = (async ({ fetch, locals: { getSession, supabase } }) => {
+export const load: LayoutServerLoad = async ({ fetch, locals: { getSession, supabase } }) => {
     return {
+        session: await getSession(),
+        user: await getUser(),
+        formRecords: await getFormRecords(), // should be not here?
         weather: (await getWeatherData())?.forecast?.forecastday[0]?.hour[0],
-        session: getSession(),
-        news: await getCMUNewsData(),
-        formRecords: await getFormRecords(),
     };
+
+    async function getUser() {
+        const session = await getSession();
+
+        if (session) {
+            const { data, error } = await supabase
+                .from("users_detail")
+                .select("*")
+                .eq("user_id", session.user.id);
+
+            // we can safely assume that this only returns 1 item
+            return data ? data[0] : null;
+        }
+    }
 
     async function getFormRecords() {
         const session = await getSession();
@@ -27,7 +41,7 @@ export const load = (async ({ fetch, locals: { getSession, supabase } }) => {
 
         const requestUrl = new URL("http://api.weatherapi.com/v1/forecast.json");
         const params = requestUrl.searchParams;
-        params.append("key", "fb9dc671848c4e9b81d120617230903");
+        params.append("key", PRIVATE_WEATHERAPI_KEY);
         params.append("q", "14.6536,120.9602");
         params.append("days", "1");
         params.append("hour", new Date().getHours().toString());
@@ -52,30 +66,4 @@ export const load = (async ({ fetch, locals: { getSession, supabase } }) => {
             return response?.status;
         }
     }
-
-    async function getCMUNewsData() {
-        let response; // response to be returned
-
-        const requestUrl = new URL(PUBLIC_CMU_WP_REST_ENDPOINT + "/posts");
-        const params = requestUrl.searchParams;
-        params.append("per_page", "12");
-
-        const controller = new AbortController();
-        const signal = controller.signal;
-
-        // Cancel the fetch request in 500ms
-        setTimeout(() => controller.abort(), 3000);
-
-        try {
-            response = await fetch(requestUrl, { signal });
-        } catch (error) {
-            console.log("There was an error:", error);
-        }
-
-        if (response?.ok) {
-            return response?.json();
-        } else {
-            return response?.status;
-        }
-    }
-}) satisfies LayoutServerLoad;
+};
